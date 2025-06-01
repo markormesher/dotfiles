0c235af30ca49b433f98dbf468ebd402bae286e4
Mark Ormesher <me@markormesher.co.uk>
2025-06-01 18:15:03 +0100

    fix format-on-save; fix css lsp name

--- files/%HOME%/.config/nvim/lua/mormesher/lsp.lua
+++ files/%HOME%/.config/nvim/lua/mormesher/lsp.lua
@@ -11,12 +11,11 @@ if (mason_ok and mason_tools_ok) then
   mason_tools.setup({
     ensure_installed = {
       -- JS/TS
-      "biome",
       "eslint-lsp",
       "typescript-language-server",
 
       -- CSS
-      "cssls",
+      "css-lsp",
 
       -- Go
       "gopls",
@@ -81,12 +80,15 @@ vim.lsp.enable({
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
     -- format on save
     if not client:supports_method("textDocument/willSaveWaitUntil") and client:supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
         callback = function()
           vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
         end,
